08b01f744afef9a4dc6f2f06c867f5bcd45a5ebb
harutiro <harutiro2027@outlook.jp>
2023-12-17 19:59:07 +0900

    add: 円形のポリゴンを表示させるようにしてみた。

--- app/src/main/java/net/harutiro/osmdroidtest/MapTapController.kt
+++ app/src/main/java/net/harutiro/osmdroidtest/MapTapController.kt
@@ -1,11 +1,14 @@
 package net.harutiro.osmdroidtest
 
+import android.graphics.Color
 import android.widget.TextView
 import org.osmdroid.events.MapEventsReceiver
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.MapEventsOverlay
 import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.Polygon
+
 
 class MapTapController(private val mapView: MapView, private val locationTextView: TextView) {
 
@@ -15,6 +18,8 @@ class MapTapController(private val mapView: MapView, private val locationTextVie
                 if (p != null) {
                     clearMarkers()
                     placeMarker(p)
+                    clearPolygon()
+                    placePolygon(p)
                     updateLocationText(p)
                 }
                 return true
@@ -34,11 +39,35 @@ class MapTapController(private val mapView: MapView, private val locationTextVie
                 }
             }
 
+            private fun clearPolygon() {
+                val overlays = mapView.overlays
+                for (i in overlays.size - 1 downTo 0) {
+                    val overlay = overlays[i]
+                    if (overlay is Polygon) {
+                        overlays.removeAt(i)
+                    }
+                }
+            }
+
             private fun placeMarker(point: GeoPoint) {
                 val marker = Marker(mapView)
                 marker.position = point
                 marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
                 mapView.overlays.add(marker)
+                mapView.invalidate()
+            }
+
+            // 円形のポリゴンを描画する
+            private fun placePolygon(point: GeoPoint) {
+
+                val geoPointList = Polygon.pointsAsCircle(point, 100.0)
+                val polygon = Polygon()
+                polygon.points = geoPointList
+                polygon.fillColor = Color.argb(75, 255, 0, 0)
+                polygon.strokeColor = Color.argb(255, 255, 0, 0)
+                polygon.strokeWidth = 2.0f
+
+                mapView.overlays.add(polygon)
                 mapView.invalidate()
             }
 
